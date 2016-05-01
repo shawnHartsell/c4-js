acf282a15467b344ebec9063423ed5329c298e88
Shawn Hartsell <shawn.hartsell@leankit.com>
2016-05-01 14:45:26 -0500

    initial implementation of game.takeTurn. Refactoring game data structure for new attributes.

--- src/game.js
+++ src/game.js
@@ -1,21 +1,33 @@
-//6x7
-import {OrderedMap, List} from 'immutable'
+import {OrderedMap, List, Map} from 'immutable'
 
 export function createNew(rows, columns){
-    var obj = {};
-    
-    for (var i = 0; i < rows; i++) {
+	var game = {};
+	game.board = buildGameBoard(rows, columns);
+	game.currentPlayerTurn = 1;
+	game.winningPlayer = undefined;
+
+	return Map(game);
+}
+
+//TODO: make use of Immutable.js helper methods for the mutations
+export function takeTurn(game, row, column, playerId){
+	var board = game.get("board");
+	var boardRow = board.get(`${row}`);
+	var updatedBoardRow = boardRow.set(`${column}`,playerId);
+	var updatedBoard = board.set(`${row}`,updatedBoardRow);
+
+	var updatedGame = game.set("currentPlayerTurn", playerId === 1 ? 2 : 1);
+	var updatedGame2 = updatedGame.set("board",updatedBoard);
+
+	return updatedGame2;
+}
+
+function buildGameBoard(rows, columns){
+	 var obj = {};
+
+     for (var i = 0; i < rows; i++) {
         obj[i] = List().setSize(columns);
-    }
+     }
 
-    var gameGrid = OrderedMap(obj);
-    return gameGrid;
-}
-
-export function executeTurn(row, column, playerId){
-
-}
-
-export function winner(){
-    
+     return OrderedMap(obj);
 }
--- test/game_spec.js
+++ test/game_spec.js
@@ -1,20 +1,36 @@
-import * as game from '../src/game';
+import * as gameApi from '../src/game';
 import {expect} from 'chai';
 
-describe('game logic', () => {
-   it('can create game board', ()=>{
+describe('game', () => {
+   it('can create game', ()=>{
         let rows = 6;
         let columns = 7;
-        
-        var board = game.createNew(6,7);
-        var entries = board.entrySeq();
-        
-        expect(entries.size).to.equal(rows);
-        
-        var hasAllColumns = entries.every(entry => {
-           return entry[1].size === columns; 
+		let game = gameApi.createNew(6,7);
+
+		expect(game.get("currentPlayerTurn")).to.equal(1);
+		expect(game.get("winningPlayer")).to.not.be.ok;
+
+		var board = game.get("board");
+		var boardRows = board.entrySeq();
+
+        expect(boardRows.size).to.equal(rows);
+
+        var hasAllColumns = boardRows.every(entry => {
+           return entry[1].size === columns;
         });
-        
+
         expect(hasAllColumns).to.equal(true);
+   });
+
+   it('can take turn with no winning move', () => {
+	   let game = gameApi.createNew(6,7);
+	   let [row,column,playerId] = [4,4,1];
+
+	   var state = gameApi.takeTurn(game, row, column, playerId);
+
+	   expect(state).to.be.ok;
+	   expect(state.get("currentPlayerTurn")).to.equal(2);
+	   expect(state.get("board").get(`${row}`).get(`${column}`)).to.equal(playerId);
+	   expect(state.get("winningPlayer")).to.not.be.ok;
    });
 });
