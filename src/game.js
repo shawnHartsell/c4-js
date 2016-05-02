83c2f90ac200a656aa5e724c660e9dc1a0fa6340
Shawn Hartsell <shawn.hartsell@leankit.com>
2016-05-01 21:13:23 -0500

    added function to produce poritional winning positions for a given move. added notes on refactoring improvements.

--- src/game.js
+++ src/game.js
@@ -5,6 +5,7 @@ export function createNew(rows, columns){
 	game.board = buildGameBoard(rows, columns);
 	game.currentPlayerTurn = 1;
 	game.winningPlayer = undefined;
+	game.size = {maxRowIndex: rows - 1, maxColumnIndex: columns - 1};
 
 	return Map(game);
 }
@@ -19,6 +20,55 @@ export function takeTurn(game, row, column, playerId){
 	return updatedGame;
 }
 
+export function getWinningPositions(gameSize, lastMoveRow, lastMoveColumn, playerId){
+	let maxRowIndex = gameSize.maxRowIndex;
+	let maxColumnIndex = gameSize.maxColumnIndex;
+
+	let positions = {
+		vertical: [],
+		horizontal: [],
+		diagonal: []
+	};
+
+	for(var i = 1; i <= 3; i++){
+		let isPrevRowValid = (lastMoveRow - i >= 0);
+		let isNextRowValid = (lastMoveRow + i <= maxRowIndex);
+		let isPrevColumnValid = (lastMoveColumn - i >= 0);
+		let isNextColumnValid = (lastMoveColumn + i <= maxColumnIndex);
+
+		//vertial winning postions
+		if(isPrevRowValid){
+			positions.vertical.push({row: lastMoveRow - i, column: lastMoveColumn});
+		}
+
+		if(isNextRowValid){
+			positions.vertical.push({row: lastMoveRow + i, column: lastMoveColumn});
+		}
+
+		//horizontal winning positions
+		if(isPrevColumnValid){
+			positions.horizontal.push({row: lastMoveRow, column: lastMoveColumn - i});
+		}
+
+		if(isNextColumnValid){
+			positions.horizontal.push({row: lastMoveRow, column: lastMoveColumn + i});
+		}
+
+		//diagonal winning positions
+		if(isPrevRowValid && isPrevColumnValid){
+			positions.diagonal.push({row: lastMoveRow - i, column: lastMoveColumn - i});
+		}
+
+		if(isNextRowValid && isNextColumnValid){
+			positions.diagonal.push({row: lastMoveRow + i, column: lastMoveColumn + i});
+		}
+	}
+
+	return positions;
+
+}
+
+//TODO: refactor to 2D array
 function buildGameBoard(rows, columns){
 	 var obj = {};
 
--- test/game_spec.js
+++ test/game_spec.js
@@ -1,4 +1,5 @@
 import * as gameApi from '../src/game';
+import chai from 'chai';
 import {expect} from 'chai';
 
 describe('game', () => {
@@ -32,5 +33,41 @@ describe('game', () => {
 	   expect(state.get("currentPlayerTurn")).to.equal(2);
 	   expect(state.get("board").get(`${row}`).get(`${column}`)).to.equal(playerId);
 	   expect(state.get("winningPlayer")).to.not.be.ok;
+   });
+
+  /*
+  	TODO:
+  	Refactor so that assert doesn't rely on array element order
+	or modify data structure of getWinningPositions	*/
+   it('can determine winning positions', ()=>{
+	   let game = gameApi.createNew(6,7);
+	   let [row,column] = [2,3];
+	   let expectedPositions = {
+			vertical:[
+				{ row: 1, column: 3 },
+				{ row: 3, column: 3 },
+				{ row: 0, column: 3 },
+				{ row: 4, column: 3 },
+				{ row: 5, column: 3 }
+			],
+			horizontal:[
+				{ row: 2, column: 2 },
+				{ row: 2, column: 4 },
+				{ row: 2, column: 1 },
+				{ row: 2, column: 5 },
+				{ row: 2, column: 0 },
+				{ row: 2, column: 6 }
+			],
+			diagonal:[
+				{ row: 1, column: 2 },
+				{ row: 3, column: 4 },
+				{ row: 0, column: 1 },
+				{ row: 4, column: 5 },
+				{ row: 5, column: 6 }
+			]
+		};
+
+	   let positions = gameApi.getWinningPositions(game.get("size"), row, column);
+	   expect(positions).to.deep.equal(expectedPositions);
    });
 });
